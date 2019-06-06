3b50ba66b7f0fd1b993fe3661014a7fd25417489
Luiz Calderaro <luizcalderaro@eml.cc>
2019-06-06 22:44:02 +0100

    Improve version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_pluginskel';
-$plugin->release = '1.2.1';
+$plugin->release = '1.0.0';
 $plugin->version = 2019012000;
 $plugin->requires = 2016052300;
 $plugin->maturity = MATURITY_STABLE;
