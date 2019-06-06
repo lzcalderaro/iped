0b64b6d3e457165a309a60b8a08d9e5346b1b7ea
Luiz Calderaro <luizcalderaro@eml.cc>
2019-06-06 23:21:03 +0100

    Add simple block

---
+++ block_ipedcourses.php
@@ -0,0 +1,11 @@
+<?php
+
+class block_ipedcourses extends block_base {
+
+    public function init() {
+
+        $this->title = get_string('simplehtml', 'block_simplehtml');
+    }
+    // The PHP tag and the curly bracket for the class definition
+    // will only be closed after there is another function added in the next section.
+}
